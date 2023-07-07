d7e04d9225c26425f7697afb1878254415583e66
swsbmm <swsbmm@gmail.com>
2023-07-07 13:31:17 -0500

    Formulario

--- creador-blogs/src/App.jsx
+++ creador-blogs/src/App.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
+import { create, element, text, fragment } from 'xmlbuilder2';
 import './App.css';
 
 function App() {
   const [header, setHeader] = useState(false);
   const [menu, setMenu] = useState(false);
-  const [escribirOpcion, setEscribirOpcion] = useState('');
+  const [titulo, setTitulo] = useState('');
+  const [descripcion, setDescripcion] = useState('');
+  const [opcionesMenu, setOpcionesMenu] = useState([]);
   const [columnaPrincipal, setColumnaPrincipal] = useState(false);
   const [posts, setPosts] = useState(false);
   const [nombreAutor, setNombreAutor] = useState('');
@@ -15,183 +18,299 @@ function App() {
   const [footer, setFooter] = useState(false);
   const [imagenFooter, setImagenFooter] = useState('');
   const [textoFooter, setTextoFooter] = useState('');
+  const [contenidoPost, setContenidoPost] = useState('');
+
+  const handleAgregarOpcion = (e) => {
+    e.preventDefault()
+    setOpcionesMenu([...opcionesMenu, '']);
+  };
+
+  const handleEliminarOpcion = (e,index) => {
+    e.preventDefault()
+    const nuevasOpciones = [...opcionesMenu];
+    nuevasOpciones.splice(index, 1);
+    setOpcionesMenu(nuevasOpciones);
+  };
+
+  const handleEditarOpcion = (index, valor) => {
+    const nuevasOpciones = [...opcionesMenu];
+    nuevasOpciones[index] = valor;
+    setOpcionesMenu(nuevasOpciones);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Lógica adicional para enviar el formulario o manejar los datos
-    console.log('Datos enviados:', {
-      header,
-      menu,
-      escribirOpcion,
-      columnaPrincipal,
-      posts,
-      nombreAutor,
-      fecha,
-      nombreArticulo,
-      contenido,
-      imagen,
-      footer,
-      imagenFooter,
-      textoFooter
-    });
+
+    try {
+      // Código que puede generar una excepción
+      const xml = create({ version: '1.0', encoding: 'UTF-8' })
+      .ele('blog')
+      // TITULO
+      if(titulo.length >= 0){
+        xml.ele('titulo').txt(titulo).up()
+      }else{
+        throw new Error("¡Se generó una excepción titulo!");
+      }
+      // DESCRIPCION
+      if(descripcion.length >= 0){
+        xml.ele('descripcion').txt(descripcion).up()
+      }else{
+        throw new Error("¡Se generó una excepción descripcion!");
+      }
+      // HEADER
+      if(header){
+        const headerElement = xml.ele('header');
+        if(menu){
+          const menuElement = headerElement.ele('menu')
+          if(opcionesMenu.length > 0){
+            opcionesMenu.map(element => {
+              menuElement.ele('item').att('href', '/' + element).txt(element).up()
+            })
+          }
+        }
+      }
+      // COLUMNA PRINCIPAL
+      if(columnaPrincipal){
+        const colprinElement = xml.ele('body');
+        const contenidoElement = xml.ele('contenido')
+        if(posts){
+          const postsElement = contenidoElement.ele('post');
+          //-------------------------------------------
+          const tituloElement = postsElement.ele('titulo').txt(nombreArticulo).up();
+          const autorElement = postsElement.ele('autor').txt(nombreAutor).up();
+          const fechaElement = postsElement.ele('fecha').txt(fecha).up();
+          const contPostElement = postsElement.ele('contenido-post').txt(contenidoPost).up();
+          const imagenElement = postsElement.ele('imagen').txt(imagen).up();
+        }
+      }
+
+      //FOOTER
+      
+
+      
+      const xmlString = xml.end({ prettyPrint: true });
+      console.log(xmlString);
+    } catch (error) {
+      // Manejo de la excepción
+      console.error("Se capturó una excepción:", error.message);
+      alert("REVISA EL FORMULARIO")
+    }
+    
+
+
+    
   };
 
   return (
     <div className='formulario-container'>      
       <form className='formulario' onSubmit={handleSubmit}>
       <h2>Formulario para crear Blog</h2>
-        <div className="formulario-grupo">
-          <label htmlFor="header">Header:</label>
-          <select id="header" onChange={(e) => setHeader(e.target.value === 'si')}>
-            <option value="no">No</option>
-            <option value="si">Sí</option>
-          </select>
+        {/* TITULO */}
+        <div className="margen card">
+          <div class="card-body">
+            <div className="formulario-grupo">
+              <label htmlFor="titulo">Titulo:</label>
+              <input type="text" id="titulo" value={titulo} onChange={(e) => setTitulo(e.target.value)} />
+            </div>
+          </div>
         </div>
 
-        {header && (
+        {/* DESCRIPCION */}
+        <div className="margen card">
+          <div class="card-body">
+            <div className="formulario-grupo">
+              <label htmlFor="descripcion">Descripcion:</label>
+              <input type="text" id="descripcion" value={descripcion} onChange={(e) => setDescripcion(e.target.value)} />
+            </div>
+          </div>
+        </div>
+
+        {/* HEADER */}
+
+        <div className="margen card">
+          <div class="card-body">
+
+            <div className="formulario-grupo">
+              <label htmlFor="header">Header:</label>
+              <select className="form-select" aria-label="Default select example" id="header" onChange={(e) => setHeader(e.target.value === 'si')}>
+                <option value="no">No</option>
+                <option value="si">Sí</option>
+              </select>
+            </div>
+
+
+            {header && (
           <>
             <div className="formulario-grupo">
               <label htmlFor="menu">Menu:</label>
-              <select id="menu" onChange={(e) => setMenu(e.target.value === 'si')}>
+              <select className="form-select" aria-label="Default select example" id="menu" onChange={(e) => setMenu(e.target.value === 'si')}>
                 <option value="no">No</option>
                 <option value="si">Sí</option>
               </select>
             </div>
 
             {menu && (
-              <>
-            <div className="formulario-grupo">
-              <label htmlFor="escribirOpcion">Escribir opción 1:</label>
-              <input
-                type="text"
-                id="escribirOpcion"
-                value={escribirOpcion}
-                onChange={(e) => setEscribirOpcion(e.target.value)}
-              />
-            </div>
+              <div>
+                <button 
+                  onClick={handleAgregarOpcion}
+                  className="btn btn-outline-primary" 
+                >Agregar opción</button>
 
-            <div className="formulario-grupo">
-              <label htmlFor="escribirOpcion">Escribir opción 2:</label>
-              <input
-                type="text"
-                id="escribirOpcion"
-                value={escribirOpcion}
-                onChange={(e) => setEscribirOpcion(e.target.value)}
-              />
-            </div>
-           
-            </>
+                {opcionesMenu.map((opcion, index) => (
+                  <div key={index} className="form-group">
+                    <div key={index} className="input-group">
+                      <input 
+                        type="text" 
+                        className="form-control" 
+                        placeholder="Nombre de la pagina" 
+                        aria-label="Nombre de la pagina"
+                        aria-describedby="basic-addon2"
+                        onChange={(e) => handleEditarOpcion(index, e.target.value)}/>
+                      <div className="input-group-append">
+                        <button 
+                          className="btn btn-outline-secondary" 
+                          type="button"
+                          onClick={(e) => handleEliminarOpcion(e,index)}
+                          >Eliminar</button>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
             )}
-          </>
-        )}
+              </>
+            )}
 
-        <div className="formulario-grupo">
-          <label htmlFor="columnaPrincipal">Columna principal:</label>
-          <select
-            id="columnaPrincipal"
-            onChange={(e) => setColumnaPrincipal(e.target.value === 'si')}
-          >
-            <option value="no">No</option>
-            <option value="si">Sí</option>
-          </select>
+
+            
+          </div>
         </div>
 
-        {columnaPrincipal && (
-          <>
+
+        {/* COLUMNA PRINCIPAL */}
+
+        <div className="margen card">
+          <div className="card-body">
+            
             <div className="formulario-grupo">
-              <label htmlFor="posts">Posts:</label>
-              <select id="posts" onChange={(e) => setPosts(e.target.value === 'si')}>
+              <label htmlFor="columnaPrincipal">Columna principal:</label>
+              <select
+                className="form-select" aria-label="Default select example"
+                id="columnaPrincipal"
+                onChange={(e) => setColumnaPrincipal(e.target.value === 'si')}
+              >
                 <option value="no">No</option>
                 <option value="si">Sí</option>
               </select>
             </div>
 
-            {posts && (
+            {columnaPrincipal && (
               <>
                 <div className="formulario-grupo">
-                  <label htmlFor="nombreAutor">Nombre autor:</label>
-                  <input
-                    type="text"
-                    id="nombreAutor"
-                    value={nombreAutor}
-                    onChange={(e) => setNombreAutor(e.target.value)}
-                  />
+                  <label htmlFor="posts">Posts:</label>
+                  <select className="form-select" aria-label="Default select example" id="posts" onChange={(e) => setPosts(e.target.value === 'si')}>
+                    <option value="no">No</option>
+                    <option value="si">Sí</option>
+                  </select>
                 </div>
 
-                <div className="formulario-grupo">
-                  <label htmlFor="fecha">Fecha:</label>
-                  <input
-                    type="text"
-                    id="fecha"
-                    value={fecha}
-                    onChange={(e) => setFecha(e.target.value)}
-                  />
-                </div>
+                {posts && (
+                  <>
+                    <div className="formulario-grupo">
+                      <label htmlFor="nombreAutor">Nombre autor:</label>
+                      <input
+                        type="text"
+                        id="nombreAutor"
+                        value={nombreAutor}
+                        onChange={(e) => setNombreAutor(e.target.value)}
+                      />
+                    </div>
 
-                <div className="formulario-grupo">
-                  <label htmlFor="nombreArticulo">Nombre artículo:</label>
-                  <input
-                    type="text"
-                    id="nombreArticulo"
-                    value={nombreArticulo}
-                    onChange={(e) => setNombreArticulo(e.target.value)}
-                  />
-                </div>
+                    <div className="formulario-grupo">
+                      <label htmlFor="fecha">Fecha:</label>
+                      <input
+                        type="date"
+                        id="fecha"
+                        value={fecha}
+                        onChange={(e) => setFecha(e.target.value)}
+                      />
+                    </div>
 
-                <div className="formulario-grupo">
-                  <label htmlFor="contenido">Contenido:</label>
-                  <textarea
-                    id="contenido"
-                    value={contenido}
-                    onChange={(e) => setContenido(e.target.value)}
-                  ></textarea>
-                </div>
+                    <div className="formulario-grupo">
+                      <label htmlFor="nombreArticulo">Nombre artículo:</label>
+                      <input
+                        type="text"
+                        id="nombreArticulo"
+                        value={nombreArticulo}
+                        onChange={(e) => setNombreArticulo(e.target.value)}
+                      />
+                    </div>
 
+                    <div className="formulario-grupo">
+                      <label htmlFor="contenidoPost">ContenidoPost:</label>
+                      <textarea
+                        id="contenidoPost"
+                        value={contenidoPost}
+                        onChange={(e) => setContenidoPost(e.target.value)}
+                      ></textarea>
+                    </div>
+
+                    <div className="formulario-grupo">
+                      <label htmlFor="imagen">Imagen:</label>
+                      <input
+                        type="file"
+                        id="imagen"
+                        value={imagen}
+                        onChange={(e) => setImagen(e.target.value)}
+                      />
+                    </div>
+                  </>
+                )}
+              </>
+            )}
+          </div>
+        </div>
+
+
+        {/* FOOTER */}
+
+        <div className="margen card">
+          <div className="card-body">
+            
+            <div className="formulario-grupo">
+              <label htmlFor="footer">Footer:</label>
+              <select className="form-select" aria-label="Default select example" id="footer" onChange={(e) => setFooter(e.target.value === 'si')}>
+                <option value="no">No</option>
+                <option value="si">Sí</option>
+              </select>
+            </div>
+
+            {footer && (
+              <>
                 <div className="formulario-grupo">
-                  <label htmlFor="imagen">Imagen:</label>
+                  <label htmlFor="imagenFooter">Imagen:</label>
                   <input
                     type="file"
-                    id="imagen"
-                    value={imagen}
-                    onChange={(e) => setImagen(e.target.value)}
+                    id="imagenFooter"
+                    value={imagenFooter}
+                    onChange={(e) => setImagenFooter(e.target.value)}
+                  />
+                </div>
+
+                <div className="formulario-grupo">
+                  <label htmlFor="textoFooter">Texto:</label>
+                  <input
+                    type="text"
+                    id="textoFooter"
+                    value={textoFooter}
+                    onChange={(e) => setTextoFooter(e.target.value)}
                   />
                 </div>
               </>
             )}
-          </>
-        )}
-
-        <div className="formulario-grupo">
-          <label htmlFor="footer">Footer:</label>
-          <select id="footer" onChange={(e) => setFooter(e.target.value === 'si')}>
-            <option value="no">No</option>
-            <option value="si">Sí</option>
-          </select>
+          </div>
         </div>
 
-        {footer && (
-          <>
-            <div className="formulario-grupo">
-              <label htmlFor="imagenFooter">Imagen:</label>
-              <input
-                type="file"
-                id="imagenFooter"
-                value={imagenFooter}
-                onChange={(e) => setImagenFooter(e.target.value)}
-              />
-            </div>
-
-            <div className="formulario-grupo">
-              <label htmlFor="textoFooter">Texto:</label>
-              <input
-                type="text"
-                id="textoFooter"
-                value={textoFooter}
-                onChange={(e) => setTextoFooter(e.target.value)}
-              />
-            </div>
-          </>
-        )}
 
         <button type="submit" className="formulario-button">Enviar</button>
       </form>
